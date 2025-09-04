5f6e5f7c80917e80b0e76eef3a888b43ec3568dc
weikai <78048447+Beginneraboutlife116@users.noreply.github.com>
2025-09-05 01:35:37 +0800

    Fix App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,5 @@ import { RouterView } from 'vue-router';
 </script>
 
 <template>
-	<h1>Vue Final</h1>
-	<RouterLink to="login">Go to Test</RouterLink> |
-	<RouterLink to="signup">Go to Test</RouterLink>
-
 	<RouterView />
 </template>

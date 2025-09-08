7c7ff586ef55603a9b153fec52175b8bfde88889
weikai <78048447+Beginneraboutlife116@users.noreply.github.com>
2025-09-08 20:02:28 +0800

    Collection all utils file to src/utils

---
+++ src/utils/modal.ts
@@ -0,0 +1,19 @@
+import Swal from "sweetalert2";
+
+function showErrorMessageModal({
+	title,
+	text
+}: {
+	title: string;
+	text: string;
+}) {
+	Swal.fire({
+		title,
+		text,
+		icon: 'error',
+	});
+}
+
+export {
+	showErrorMessageModal,
+}
